12df300d1c2bec48f7da4f84475747cb42571861
ahmadalzoubi31 <ahmadalzoubi31@gmail.com>
2024-11-11 20:45:22 +0300

    modified:   server/api/buildings/index.post.js

--- server/api/buildings/index.post.js
+++ server/api/buildings/index.post.js
@@ -1,16 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   try {
-    const res = await prisma.building.create({ data: body });
-    return res;
+    await prisma.building.create({ data: body });
   } catch (error) {
-    throw createError({
-      statusCode: 500,
-      message: error.message,
-    });
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (error.code === "P2002") {
+        var msg = "There is a unique constraint violation, a new user cannot be created with this email";
+        console.log(msg);
+        throw createError({
+          statusCode: 500,
+          message: msg,
+        });
+      }
+    }
+
+    // console.log(error);
+    //   throw createError({
+    //     statusCode: 500,
+    //     message: error.message,
+    //   });
   }
 });
