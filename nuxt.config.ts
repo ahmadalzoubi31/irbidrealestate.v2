b6ad1c6e4135d98e16a16689b3e8487a4e7b55ea
ahmadalzoubi31 <ahmadalzoubi31@gmail.com>
2024-11-25 12:40:03 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: false },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
   colorMode: {
     preference: "light",
   },
