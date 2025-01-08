dd910b7230305f5b072f1d17d9d3a23b8e0ff874
ahmadalzoubi31 <ahmadalzoubi31@gmail.com>
2025-01-08 16:30:33 +0300

    modified:   pages/apartments/payments.vue

--- pages/apartments/payments.vue
+++ pages/apartments/payments.vue
@@ -46,8 +46,8 @@ const deleteSelectedRecord = async () => {
         <!-- Action Buttons & Search Filter -->
         <div class="flex my-3 justify-between">
           <div id="buttonWrapper">
-            <UButton icon="i-heroicons-plus-circle-20-solid" label="اضافة بناية" :to="'/apartments/payments/create'" />
-            <UButton icon="i-heroicons-minus-circle-20-solid" label="حذف بناية" @click="deleteSelectedRecord" />
+            <UButton icon="i-heroicons-plus-circle-20-solid" label="اضافة دفعة" :to="'/apartments/payments/create'" />
+            <UButton icon="i-heroicons-minus-circle-20-solid" label="حذف دفعة" @click="deleteSelectedRecord" />
           </div>
           <UInput class="w-1/6" v-model="q" placeholder="البحث ..." />
         </div>
