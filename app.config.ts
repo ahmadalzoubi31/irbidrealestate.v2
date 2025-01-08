f653e36e1afbefcdd11e72af83686093c95e5cd3
ahmadalzoubi31 <ahmadalzoubi31@gmail.com>
2025-01-08 17:15:42 +0300

    modified:   app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,10 +2,7 @@ export default defineAppConfig({
   ui: {
     primary: "lime",
     gray: "neutral",
-    notifications: {
-      // Show toasts at the top right of the screen
-      position: "top-0 bottom-[unset]",
-    },
+
     table: {
       default: {
         loadingState: {
