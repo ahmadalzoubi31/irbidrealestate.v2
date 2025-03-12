a212be9916e764658af8d232b9fa53ae629897e9
ahmadalzoubi31 <ahmadalzoubi31@gmail.com>
2025-03-12 23:38:06 +0300

    modified:   server/api/upload/index.delete.ts

--- server/api/upload/index.delete.ts
+++ server/api/upload/index.delete.ts
@@ -3,23 +3,23 @@ import prisma from "~/lib/prisma";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  await prisma.$transaction(async (tx) => {
-    const deleteOperations = body.files.map((idToDelete: number) => tx.appFile.delete({ where: { id: idToDelete } }));
+  // await prisma.$transaction(async (tx) => {
+  //   const deleteOperations = body.files.map((idToDelete: number) => tx.appFile.delete({ where: { id: idToDelete } }));
 
-    const appFileList = body.files.map(async (appFileId: number) => {
-      console.log({ appFileId });
-      const t = await tx.appFile.findFirst({ where: { id: appFileId } });
-      return t;
-    });
+  //   const appFileList = body.files.map(async (appFileId: number) => {
+  //     console.log({ appFileId });
+  //     const t = await tx.appFile.findFirst({ where: { id: appFileId } });
+  //     return t;
+  //   });
 
-    console.log("🚀 ~ await prisma.$transaction ~ appFileList:", appFileList);
+  //   console.log("🚀 ~ await prisma.$transaction ~ appFileList:", appFileList);
 
-    // from appFileList, get the id of the fileContent and delete it
-    const fileContentIds = await Promise.all(appFileList).then((files) => files.map((file) => file?.fileContentId));
+  //   // from appFileList, get the id of the fileContent and delete it
+  //   const fileContentIds = await Promise.all(appFileList).then((files) => files.map((file) => file?.fileContentId));
 
-    const deleteContentOperations = fileContentIds.map((idToDelete: number) => tx.fileContent.delete({ where: { id: idToDelete } }));
+  //   const deleteContentOperations = fileContentIds.map((idToDelete: number) => tx.fileContent.delete({ where: { id: idToDelete } }));
 
-    // Execute deletions, updates, and creations
-    await Promise.all([...deleteOperations, ...deleteContentOperations]);
-  });
+  //   // Execute deletions, updates, and creations
+  //   await Promise.all([...deleteOperations, ...deleteContentOperations]);
+  // });
 });
